b15bb250b2af24e2c65161a25cdef0a1f8faf8a3
Nicholas Yang <nicholsyoung25@gmail.com>
2026-09-15 12:42:58 +0700

    feat: Sitemap Update

---
+++ app/sitemap.ts
@@ -0,0 +1,20 @@
+import { MetadataRoute } from 'next'
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  const baseUrl = process.env.NEXT_PUBLIC_APP_URL || 'https://www.nyenyak.com'
+
+  return [
+    {
+      url: `${baseUrl}`,
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 1,
+    },
+    {
+      url: `${baseUrl}/booking`,
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.8,
+    },
+  ]
+}
